1ebe55739971cbc1c1217fff414c3bbad687c7df
Nimishakavi Sri Nihal <nimishakavisrinihal@gmail.com>
2026-04-24 15:37:14 +0530

    Update API URL to use environment variable

--- frontend/src/components/ArticleById.jsx
+++ frontend/src/components/ArticleById.jsx
@@ -47,7 +47,11 @@ function ArticleByID() {
       setLoading(true);
 
       try {
-        const res = await axios.get(`http://localhost:4060/user-api/article/${id}`, { withCredentials: true });
+        // const res = await axios.get(`http://localhost:4060/user-api/article/${id}`, { withCredentials: true });
+        const res = await axios.get(
+  `${import.meta.env.VITE_API_URL}/user-api/article/${id}`,
+  { withCredentials: true }
+);
 
         setArticle(res.data.payload);
       } catch (err) {
